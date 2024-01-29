6bb4108ba6f983a0a4f8a390ae55fa757c781216
Thomash07 <Thomash0720@gmail.com>
2024-01-29 18:00:34 -0500

    fixed errors - reuseable searches

--- books/src/routes/+page.svelte
+++ books/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import axios from 'axios';
 	import type { APIResponse, Item } from './types';
+	import '../styles/mainpage.css';
+	import { destroy_component } from 'svelte/internal';
 
 	let search = '';
 	let bookData: Item[] = [];
@@ -67,7 +69,11 @@
 				<div class="book-item" id="tooltip">
 					<div id="tooltiptext">
 						<span class="title">{book.volumeInfo.title}</span>
-						<span class="author">by {book.volumeInfo.authors.join(', ')}</span>
+						{#if book.volumeInfo.authors != undefined}
+							<span class="author">by {book.volumeInfo.authors.join(', ')}</span>
+						{:else}
+							<span class="author">Unknown</span>
+						{/if}
 						<p>
 							Page Count: {book.volumeInfo.pageCount}
 							<br />
@@ -91,127 +97,29 @@
 							{/if}
 						</p>
 					</div>
-
+					{#if book.volumeInfo.imageLinks != undefined}
 					<img src={book.volumeInfo.imageLinks.thumbnail} alt="" />
-					<h3 class="title">{book.volumeInfo.title}</h3>
-					<p class="author">{book.volumeInfo.authors.join(', ')}</p>
-					{#if book.volumeInfo.description.length >= 500}
-						<p class="desc">{book.volumeInfo.description.slice(0, 500)}...</p>
 					{:else}
-						<p class="desc">{book.volumeInfo.description}</p>
+					<img src = '/Logo.png' alt="">
+					{/if}
+					<h3 class="title">{book.volumeInfo.title}</h3>
+					{#if book.volumeInfo.authors != undefined}
+						<p class="author">{book.volumeInfo.authors.join(', ')}</p>
+					{:else}
+						<p class = "author">Unknown</p>
+					{/if}
+
+					{#if book.volumeInfo.description != undefined}
+						{#if book.volumeInfo.description.length >= 500}
+							<p class="desc">{book.volumeInfo.description.slice(0, 500)}...</p>
+						{:else}
+							<p class="desc">{book.volumeInfo.description}</p>
+						{/if}
+						{:else}
+							<p class="desc"> No description avaliable</p>
 					{/if}
 				</div>
 			{/each}
 		</div>
 	</div>
 </div>
-
-<style>
-	#app {
-		display: flex;
-		min-height: 100vh;
-		min-width: 100vw;
-	}
-	h1 {
-		font-family: 'Indie Flower', cursive;
-		font-size: xxx-large;
-		color: rgb(117, 72, 20);
-		background-color: rgb(219, 204, 186);
-		border-radius: 20px;
-		margin: 10%;
-		border-style: dashed;
-		border-width: 5px;
-	}
-	.right {
-		float: right;
-		width: 85%;
-	}
-
-	.left {
-		width: 15%;
-		float: left;
-		text-align: center;
-		background-image: url('./src/img/books.jpg');
-	}
-
-	.search {
-		padding: 25px;
-		text-align: center;
-	}
-	input {
-		border-radius: 15px;
-		border-color: rgb(133, 105, 29);
-		padding: 1%;
-		font-family: 'Montserrat', sans-serif;
-		width: 80%;
-		text-align: center;
-	}
-
-	button {
-		background-color: transparent;
-		border: none;
-		cursor: pointer;
-	}
-
-	.book-container {
-		display: grid;
-		grid-template-columns: repeat(3, minmax(200px, 0.5fr));
-		grid-gap: 30px;
-		padding: 5%;
-	}
-
-	.book-item {
-		background-color: rgb(252, 247, 241);
-		border-radius: 20px;
-		padding: 5%;
-		text-align: center;
-		font-family: 'Montserrat', sans-serif;
-	}
-
-	#tooltip {
-		position: relative;
-		display: block;
-	}
-
-	#tooltip #tooltiptext {
-		visibility: hidden;
-		width: 75%;
-		height: auto;
-		background-color: rgb(250, 250, 250);
-
-		padding: 2%;
-		color: #000000;
-		text-align: center;
-		text-align: left;
-		font-size: small;
-		font-family: 'Montserrat', sans-serif;
-
-		border-radius: 20px;
-		border-style: solid;
-		border-width: 5px;
-		border-color: rgb(117, 72, 20);
-
-		position: absolute;
-		z-index: 1;
-		top: 5px;
-		left: 102%;
-	}
-
-	#tooltip:hover #tooltiptext {
-		visibility: visible;
-	}
-
-	.title {
-		margin-bottom: 0%;
-		font-weight: 1000;
-	}
-
-	.desc {
-		text-align: left;
-		font-size: small;
-	}
-	.author {
-		font-size: small;
-		font-style: italic;
-	}
-</style>
