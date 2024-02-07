807952815148ffb8a97f6018d01a2b9fd0fcc979
Thomash07 <Thomash0720@gmail.com>
2024-02-06 23:10:23 -0500

    about

--- books/src/routes/+page.svelte
+++ books/src/routes/+page.svelte
@@ -1,6 +1,130 @@
 <script lang="ts">
-	import Authenticate from "../components/Authenticate.svelte";
+	import axios from 'axios';
+	import type { APIResponse, Item } from './types';
+	import '../styles/mainpage.css';
+	import { destroy_component } from 'svelte/internal';
+	let search = '';
+	let bookData: Item[] = [];
 
+	function inputChange(event: Event) {
+		search = (event.target as HTMLInputElement).value;
+	}
+
+	function searchBook(event: KeyboardEvent) {
+		if (event.key === 'Enter') {
+			axios
+				.get(
+					'https://www.googleapis.com/books/v1/volumes?q=' +
+						search +
+						'&key=AIzaSyBG3tXPoXEbCAvMw9YK3eEMeRXj0vtj-LM&maxResults=10'
+				)
+				.then((res) => {
+					// This will get the responses data and cast it
+					// to an APIResponse interface.
+					const data: APIResponse = res.data;
+					bookData = data.items;
+				})
+				.catch((err) => console.log(err));
+		}
+	}
+
+	function searchBookButton(event: MouseEvent) {
+		axios
+			.get(
+				'https://www.googleapis.com/books/v1/volumes?q=' +
+					search +
+					'&key=AIzaSyBG3tXPoXEbCAvMw9YK3eEMeRXj0vtj-LM&maxResults=10'
+			)
+			.then((res) => {
+				// This will get the responses data and cast it to an APIResponse interface.
+				const data: APIResponse = res.data;
+				bookData = data.items;
+			})
+			.catch((err) => console.	log(err));
+		
+	}
 </script>
 
-<Authenticate/>
+<div id="app">
+	<div class="left">
+		<div class = "title">
+			<h1>Book Search</h1>
+		</div>
+		<div class= "sidebar">
+			<h1>about us</h1>
+		</div>
+	</div>
+
+	<div class="right">
+		<div class="search">
+			<input
+				type="text"
+				placeholder="Enter Book Name"
+				bind:value={search}
+				on:input={inputChange}
+				on:keypress={searchBook}
+			/>
+			<button on:click={searchBookButton}
+				><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;" /></button
+			>
+		</div>
+
+		<div class="book-container">
+			{#each bookData as book, i}
+				<div class="book-item" id="tooltip">
+					<div id="tooltiptext">
+						<span class="title">{book.volumeInfo.title}</span>
+						{#if book.volumeInfo.authors != undefined}
+							<span class="author">by {book.volumeInfo.authors.join(', ')}</span>
+						{:else}
+							<span class="author">Unknown</span>
+						{/if}
+						<p>
+							Page Count: {book.volumeInfo.pageCount}
+							<br />
+							Publisher:
+							{#if book.volumeInfo.publisher === undefined}
+								Not Available
+							{:else}
+								{book.volumeInfo.publisher}
+							{/if}
+							<br />
+							Genre: {book.volumeInfo.categories}
+							<br />
+							Language: {book.volumeInfo.language}
+							<br />
+
+							Price:
+							{#if book.saleInfo.retailPrice?.amount === undefined}
+								Not Available
+							{:else}
+								{book.saleInfo.retailPrice?.amount} USD
+							{/if}
+						</p>
+					</div>
+					{#if book.volumeInfo.imageLinks != undefined}
+					<img src={book.volumeInfo.imageLinks.thumbnail} alt="" />
+					{:else}
+					<img src = '/Logo.png' alt="">
+					{/if}
+					<h3 class="title">{book.volumeInfo.title}</h3>
+					{#if book.volumeInfo.authors != undefined}
+						<p class="author">{book.volumeInfo.authors.join(', ')}</p>
+					{:else}
+						<p class = "author">Unknown</p>
+					{/if}
+
+					{#if book.volumeInfo.description != undefined}
+						{#if book.volumeInfo.description.length >= 500}
+							<p class="desc">{book.volumeInfo.description.slice(0, 500)}...</p>
+						{:else}
+							<p class="desc">{book.volumeInfo.description}</p>
+						{/if}
+						{:else}
+							<p class="desc"> No description avaliable</p>
+					{/if}
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
---
+++ books/src/routes/about/+page.svelte
@@ -0,0 +1,68 @@
+<div class ="about">
+    <div class = "model">
+        <h1>About Us</h1>
+        <div class = "content">
+            Hello! welcome to the Book Search about page.
+            <div class = "text">
+                This site allows you to search up a variety of books 
+                through the use of Google Books APIs. We also have a 
+                authentication system from Vites APIs integreted with 
+                Google OAuth (Open Authentication). 
+            </div>
+            <div class = "apis">
+                <h2>API</h2>
+            </div>
+
+            <div class = "contacts">
+                <h2>Creators</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .content{
+        
+        font-family: 'Indie Flower', cursive;
+        color: rgb(117, 72, 20);
+        font-size:24px;
+
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+    .text{
+        width: 75%;
+    }
+    h1{
+        font-family: 'Indie Flower', cursive;
+		font-size: xxx-large;
+		color: rgb(117, 72, 20);
+        background-color: rgb(219, 204, 186);
+        font-weight:bolder;
+        border-radius: 20px;
+		margin: 10px;
+		border-style: dashed;
+		border-width: 5px;
+
+        display:flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .about{
+        display:flex;
+        justify-content: center;
+        align-items:center;
+    }
+    .model{
+        text-align: center;
+        width:1500px;
+        height:900px;
+        background-color: rgb(219, 204, 186);
+        border-radius: 20px;
+        margin: 10px;
+        border-width: 5px;
+        box-shadow: 2px 4px 16px rgb(0 0 0 / .2);
+    }
+</style>
