5244566ed4dc459a101b4e788cde329f52dab9d8
Justin Wong <justinw21@macbook-pro.mynetworksettings.com>
2024-02-06 22:54:18 -0500

    fixed some formatting and google auth

--- books/src/store/store.ts
+++ books/src/store/store.ts
@@ -4,14 +4,16 @@ import { writable } from 'svelte/store';
 import type { User } from 'firebase/auth';
 import { goto } from '$app/navigation';
 
-export const authStore = writable<{ user : User | null}>({
+export const authStore = writable<{
+     user : User | null;
+}>({
     user: null
 })
 
 export const authHandlers = {
     logout: async () => {
-        await auth.signOut();
-        goto("/");
+            await auth.signOut();
+            goto("/");
     },
     loginWithGoogle: async () => {
         await signInWithPopup(auth, googleProvider);
